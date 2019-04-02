5125f055f6c37da3bf22d2ec4e6fcff2342ef7c2
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-02 14:27:35 -0400

    refactor fetch drills to use a store and render pattern

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 'use strict';
 
-function fetchtotaldog() {
-  $('.form-num').on('submit', function(event){
+const store = [];
+
+function fetchDogImages(totalImages) {
+  console.log('totalImages', totalImages);
+  if (!totalImages){
+    console.log('here');
+    totalImages = 3;
+  }
+
+  const apiUrl = `https://dog.ceo/api/breeds/image/random/${totalImages}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      jsonData.message.forEach(img => store.push(img));
+      render();
+    })
+    .catch((error) => console.log(error));
+}
+
+function render() {
+  const dogTemplate = store.map(buildDogHtml);
+  $('.js-dog-results').html(dogTemplate);
+}
+
+function fetchDogs() {
+  $('form#total-dogs').on('submit', function(event){
     event.preventDefault();
-    let apiUrl='';
-    const resultTotal = $(this).serialize().split('=')[1];
-    if (resultTotal){
-      apiUrl=`https://dog.ceo/api/breeds/image/random/${resultTotal}`;}
-    else{
-      apiUrl=`https://dog.ceo/api/breeds/image/random/3`;
-    }
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((jsonData) => {
-        const dogImages = jsonData.message;
-        const dogTemplate = dogImages.map(buildDogHtml);
-        $('.js-dog-results').html(dogTemplate);
-      })
-      .catch((error) => console.log(error));
-    
+    event.target.reset();
+    const totalImages = $('#number-of-dogs').val();
+    fetchDogImages(totalImages);
   });
 }
+
 function breed(){
   $('.form-breed').on('submit', function(event){
     event.preventDefault();
     let breed = $('.breed-name').val();
     let urlapi= `https://dog.ceo/api/breed/${breed}/images/random`;
     let xhr = new XMLHttpRequest();
-    try{urlapi=`https://dog.ceo/api/breed/hound/images/random`;
-    fetch(urlapi).then(res=>res.json()).then(data=>{
-      $('.js-dog-results').html(buildDogHtml(data.message));
-    }).catch((error)=>
-      console.log(error)
-  )} catch(e){fetch('https://dog.ceo/api/breed/hound/images/random').then(res=>res.json()).then(data=>{$('.js-dog-results').html(buildDogHtml(data.message));}).catch((error)=>console.log(error));}
-  //   fetch('https://dog.ceo/api/breed/hound/images/random').then(res=>res.json()).then(data=>{$('.js-dog-results').html(buildDogHtml(data.message));}).catch((error)=>console.log(error));
-  
-
-})}
+    try {
+      urlapi=`https://dog.ceo/api/breed/hound/images/random`;
+      fetch(urlapi).then(res=>res.json()).then(data=>{
+        $('.js-dog-results').html(buildDogHtml(data.message));
+      }).catch((error) => console.log(error))
+    } catch(e){
+      fetch('https://dog.ceo/api/breed/hound/images/random').then(res=>res.json()).then(data=>{$('.js-dog-results').html(buildDogHtml(data.message));}).catch((error)=>console.log(error));
+    }
+  });
+}
 
 function buildDogHtml(image) {
   return `
@@ -46,7 +59,7 @@ function buildDogHtml(image) {
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
-  fetchtotaldog();
+  fetchDogs();
   breed();
   breedlist();
 });
