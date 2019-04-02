6f475a5cf0ec92dc3555e3d5ed4c6f166dc0dd23
hhcgit <hhc50331994@gmail.com>
2019-04-02 10:19:25 -0700

    done fixing

--- index.js
+++ index.js
@@ -25,17 +25,15 @@ function breed(){
   $('.form-breed').on('submit', function(event){
     event.preventDefault();
     let breed = $('.breed-name').val();
-    console.log(breed);
-    if(breed!==''){
     let urlapi= `https://dog.ceo/api/breed/${breed}/images/random`;
+    let xhr = new XMLHttpRequest();
+    try{urlapi=`https://dog.ceo/api/breed/hound/images/random`;
     fetch(urlapi).then(res=>res.json()).then(data=>{
       $('.js-dog-results').html(buildDogHtml(data.message));
     }).catch((error)=>
       console.log(error)
-  )
-  }else{
-    fetch('https://dog.ceo/api/breed/hound/images/random').then(res=>res.json()).then(data=>{$('.js-dog-results').html(buildDogHtml(data.message));}).catch((error)=>console.log(error));
-  }
+  )} catch(e){fetch('https://dog.ceo/api/breed/hound/images/random').then(res=>res.json()).then(data=>{$('.js-dog-results').html(buildDogHtml(data.message));}).catch((error)=>console.log(error));}
+  //   fetch('https://dog.ceo/api/breed/hound/images/random').then(res=>res.json()).then(data=>{$('.js-dog-results').html(buildDogHtml(data.message));}).catch((error)=>console.log(error));
   
 
 })}
@@ -50,4 +48,5 @@ $(function() {
   console.log('App loaded! Waiting for submit!');
   fetchtotaldog();
   breed();
+  breedlist();
 });
